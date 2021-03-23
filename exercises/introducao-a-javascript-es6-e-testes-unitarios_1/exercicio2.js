32dc59b55d54e1f0e7ff6bd70832878e88288215
lucas-paz <lucas.hpaz@outlook.com>
2021-03-23 20:52:53 -0300

    Adiciona função de compara palavras

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio2.js
@@ -1,9 +1,16 @@
 const fatorial = numero => {
     let resultado = 1;
-  for (let index = 0; index < numero; index += 1 ) {
-      resultado += resultado * index;
-  }
-  return resultado;
+    for (let index = 0; index < numero; index += 1) {
+        resultado += resultado * index;
+    }
+    return resultado;
 }
 
-console.log(fatorial(5));
+console.log(fatorial(3));
+
+const maiorPalavra = string => string.split(' ').sort((palavra1, palavra2) => palavra2.length - palavra1.length)[0];
+// separa string em palvras a cada espaço -> organizar as palavras em ordem decrescente ->
+// Retorna primeiro valor do array (Maior palavra)
+// a função dentro do sort compara palavra a palavra e as organiza (Analise Combinatória)
+
+console.log(maiorPalavra("Antônio foi no banheiro e não sabemos o que aconteceu"));
