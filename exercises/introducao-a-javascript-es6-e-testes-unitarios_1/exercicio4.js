e1aef62b1d617b3a194608c9e7e35292c13fa962
lucas-paz <lucas.hpaz@outlook.com>
2021-03-23 21:58:42 -0300

    Adiciona parte 2 do exercicio 4

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio4.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio4.js
@@ -1,4 +1,13 @@
-const searchX = (word, string) => string.includes('x')? string.replace(/x/i, word) : string;
+let skills = ['JS', 'HTML', 'CSS', 'GIThub', 'Phyton'];
 
-console.log(searchX('Lucas','Hora de descansar x'));
-  
+const searchX = (word, string) => string.includes('x') ? string.replace(/x/i, word) : string;
+let text = searchX('Lucas', 'Hora de descansar x');
+
+const concatAll = string => {
+    let result = `${string}, minhas 5 principais habilidades são:`;
+    for (let index = 0; index < skills.length; index += 1) {
+        result.concat(skills[index]);
+    }
+}
+
+console.log(concatAll(text));
