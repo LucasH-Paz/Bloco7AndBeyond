284998ec9ca976f246ec3e945de2b26a6428ea8f
lucas-paz <lucas.hpaz@outlook.com>
2021-03-29 19:04:39 -0300

    Adiciona exercicio 4

--- exercises/higher-order-functions-do-javascript-es6_1/b8d1.js
+++ exercises/higher-order-functions-do-javascript-es6_1/b8d1.js
@@ -14,10 +14,20 @@ const newEmployees = (name) => {
     employee['id' + (size + 1)] = newEployeId(name);
 }
 
-const verify = (number) => number == Math.round(Math.random()*5);
+const verify = (number) => number == Math.round(Math.random() * 5);
 
-const luckOrNot = (bet) => verify(bet)? 'Parabens, você ganhou' : 'Tente Novamente';
+const luckOrNot = (bet) => verify(bet) ? 'Parabens, você ganhou' : 'Tente Novamente';
 
-console.log(luckOrNot(0));
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const exam = (answer) => {}
+const check = (arr1, arr2) => {
+    let points = 0;
+    for (let index = 0; index < arr1.length; index += 1) {
+        (arr1[index] === arr2[index]) ? points += 1 : points -= 0.5;
+        if (arr2[index] === 'N.A') { points += 0, 5 };
+    }
+    return points;
+}
+
+const checkAnswers = (param1, param2, func) => func(param1, param2);
